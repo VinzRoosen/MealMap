707a620fba43c4ac5756423e39bf765bc70b6d9a
VinzRoosen <vinz.roosen@skynet.be>
2023-03-22 19:46:58 +0100

    NewBtn en EditBtn toegevoegd

--- app/src/main/java/be/LarsVinz/OurApp/MainActivity.kt
+++ app/src/main/java/be/LarsVinz/OurApp/MainActivity.kt
@@ -2,10 +2,23 @@ package be.LarsVinz.OurApp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import be.LarsVinz.OurApp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //Edit Recept
+        binding.btnEdit.setOnClickListener{
+        }
+
+        //New Recept
+        binding.btnNew.setOnClickListener{
+        }
+
     }
 }
