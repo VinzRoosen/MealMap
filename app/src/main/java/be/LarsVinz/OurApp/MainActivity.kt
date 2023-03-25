8409b4eca110fa95fbdfd5312875db8621c78a2f
Lars Gielen <larsgielen2003@gmail.com>
2023-03-25 14:42:05 +0100

    Added menuBar

--- app/src/main/java/be/LarsVinz/OurApp/MainActivity.kt
+++ app/src/main/java/be/LarsVinz/OurApp/MainActivity.kt
@@ -2,23 +2,52 @@ package be.LarsVinz.OurApp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
 import be.LarsVinz.OurApp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
     private lateinit var binding: ActivityMainBinding
+    private lateinit var menuBarToggle: ActionBarDrawerToggle
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setupMainFragment()
+        setupMenuDrawer()
+
         setContentView(binding.root)
+    }
 
-        //Edit Recept
-        binding.btnEdit.setOnClickListener{
+    private fun setupMainFragment() {
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.mainContainer, MainFragment())
+            commit()
         }
+    }
 
-        //New Recept
-        binding.btnNew.setOnClickListener{
+    private fun setupMenuDrawer() {
+        menuBarToggle = ActionBarDrawerToggle(this, binding.drawerLayout, R.string.open_menu, R.string.close_menu)
+        binding.drawerLayout.addDrawerListener(menuBarToggle)
+        menuBarToggle.syncState()
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        
+        binding.navView.setNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.menuTest -> println("Test")
+            }
+            true
         }
+    }
 
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // we need to do this to respond correctly to clicks on menu items, otherwise it won't be caught
+        if(menuBarToggle.onOptionsItemSelected(item)) {
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
