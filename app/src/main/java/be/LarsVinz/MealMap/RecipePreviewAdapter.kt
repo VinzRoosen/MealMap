303c2784289c097b8edf49dc8e1ad58460bf243f
VinzRoosen <vinz.roosen@skynet.be>
2023-03-30 13:27:18 +0200

    RecipeListFragment

--- app/src/main/java/be/LarsVinz/MealMap/RecipePreviewAdapter.kt
+++ app/src/main/java/be/LarsVinz/MealMap/RecipePreviewAdapter.kt
@@ -3,25 +3,19 @@ package be.LarsVinz.MealMap
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-<<<<<<< HEAD
+
 import android.widget.Button
-import android.widget.TextView
 import androidx.core.os.bundleOf
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
-=======
 import android.widget.TextView
->>>>>>> ea6be0ea979ebe5da512e12e74909e9b66ea304b
+
 import androidx.recyclerview.widget.RecyclerView
 
 class RecipePreviewAdapter (val items: MutableList<Recipe>): RecyclerView.Adapter<RecipePreviewAdapter.testViewHolder>(){
     inner class testViewHolder(currentItemView: View) : RecyclerView.ViewHolder(currentItemView)
 
-<<<<<<< HEAD
 
-
-=======
->>>>>>> ea6be0ea979ebe5da512e12e74909e9b66ea304b
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): testViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.item_recipe, parent, false)
         return testViewHolder(view)
@@ -31,7 +25,6 @@ class RecipePreviewAdapter (val items: MutableList<Recipe>): RecyclerView.Adapte
         val currentItem = items[position]
         holder.itemView.apply {
             findViewById<TextView>(R.id.txtRecipe).text = currentItem.recipeName
-<<<<<<< HEAD
             findViewById<TextView>(R.id.txtRecipe).setOnClickListener{
                 System.out.println("Als dit print werkt de knop")
 
@@ -44,8 +37,6 @@ class RecipePreviewAdapter (val items: MutableList<Recipe>): RecyclerView.Adapte
 
                 findNavController().navigate(R.id.action_recipeListFragment_to_recipeDetailFragment, bundle)
             }
-=======
->>>>>>> ea6be0ea979ebe5da512e12e74909e9b66ea304b
         }
     }
 
